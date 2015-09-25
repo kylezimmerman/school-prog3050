3e44a08e04f2d7fda410acba4733102e72a20e64
DAmatheson <row.blood.eagle@gmail.com>
2015-09-25 12:11:30 -0400

    Initial Solution Commit

---
+++ Veil/Veil/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Veil.MvcApplication" Language="C#" %>
---
+++ Veil/Veil/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Veil.Startup))]
+namespace Veil
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
