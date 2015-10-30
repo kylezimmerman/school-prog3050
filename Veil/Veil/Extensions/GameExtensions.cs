6016af343bc1e20b150cab311fc6aec5abeef4e1
KyleZimmerman <zimmerkyle@gmail.com>
2015-10-30 15:04:01 -0400

    Game List Update
    - Added stars to game list
    - Added GameExtensions for helper functions related to game (e.g. GetAverageRating())
    - Added FontAwesome
    - Created Veil.Extensions and moved CountryExtensions and IIdentityExtensions. This effected several pages that had includes to it.

---
+++ Veil/Veil/Extensions/GameExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Veil.DataModels.Models;
+
+namespace Veil.Extensions
+{
+    public static class GameExtensions
+    {
+        public static double? GetAverageRating(this Game game)
+        {
+            return game.GameSKUs.SelectMany(g => g.Reviews).Average(r => (double?)r.Rating);
+        }
+    }
+}
