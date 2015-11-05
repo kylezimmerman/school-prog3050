29ed9ef4dc49a3cda23cf929a67030a3c87f8427
Isaac West <bokthaku@gmail.com>
2015-11-05 10:22:17 -0500

    #39 Moved contents of foreach to a partial

---
+++ Veil/Veil/Models/WishlistProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veil.DataModels.Models;
+
+namespace Veil.Models
+{
+    public class WishlistPhysicalGameProductViewModel
+    {
+        public PhysicalGameProduct Product { get; set; }
+        public bool MemberIsCurrentUser { get; set; }
+    }
+}
