1d2ba0402fd82fcf92ad0dfbdcc1e0da93dd88ac
Sean Coombes <seancoombes@live.ca>
2015-11-10 10:06:30 -0500

    added deletephysicalgameproduct test

--- Veil/Veil.Tests/Controllers/GamesControllerTests.cs
+++ Veil/Veil.Tests/Controllers/GamesControllerTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using Moq;
 using NUnit.Framework;
@@ -9,6 +10,7 @@ using Veil.Controllers;
 using Veil.DataAccess.Interfaces;
 using Veil.DataModels;
 using Veil.DataModels.Models;
+using Veil.Models;
 
 namespace Veil.Tests.Controllers
 {
@@ -776,6 +778,37 @@ namespace Veil.Tests.Controllers
             Assert.That(result != null);
             Assert.That(result.Model, Is.Not.Null);
             Assert.That(result.Model, Is.InstanceOf<GameProduct>());
+        }
+
+        [TestCase(VeilRoles.MEMBER_ROLE)]
+        [TestCase(VeilRoles.ADMIN_ROLE)]
+        public async void DeletePhysicalGameProduct_InvalidID(string role)
+        {
+            GameProduct gameSku = new PhysicalGameProduct();
+            gameSku.GameId = Id;
+            gameSku.Id = Id;
+
+
+
+            Guid badId = new Guid("44B0752E - 998B - 477A - AAAD - 3ED535BA3559");
+
+            Mock<IVeilDataAccess> dbStub = TestHelpers.GetVeilDataAccessFake();
+            Mock<DbSet<GameProduct>> gameProductDbSetStub = TestHelpers.GetFakeAsyncDbSet(new List<GameProduct> { gameSku }.AsQueryable());
+            gameProductDbSetStub.SetupForInclude();
+
+            gameProductDbSetStub.Setup(gp => gp.FindAsync(Id)).ReturnsAsync(gameSku);
+            dbStub.Setup(db => db.GameProducts).Returns(gameProductDbSetStub.Object);
+
+
+            Mock<ControllerContext> contextStub = new Mock<ControllerContext>();
+            contextStub.SetupUser().IsInRole(role);
+
+            GamesController controller = new GamesController(dbStub.Object)
+            {
+                ControllerContext = contextStub.Object
+            };
+
+            Assert.That(async () => await controller.DeleteGameProduct(badId), Throws.InstanceOf<HttpException>().And.Matches<HttpException>(e => e.GetHttpCode() == 404));
         }
     }
 }
