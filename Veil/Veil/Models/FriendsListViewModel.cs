2c0e3143dda91e5499352647607b2b84959c8055
Justin Coschi <jcoschi@hotmail.com>
2015-11-10 20:02:13 -0500

    #40, #41 - basic logic for creating and processing Friendship requests

---
+++ Veil/Veil/Models/FriendsListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veil.DataModels.Models;
+
+namespace Veil.Models
+{
+    public class FriendsListViewModel
+    {
+        public List<Member> PendingSentFriendships { get; set; }
+        public List<Member> PendingReceivedFriendships { get; set; }
+        public List<Member> ConfirmedFriends { get; set; }
+    }
+}
