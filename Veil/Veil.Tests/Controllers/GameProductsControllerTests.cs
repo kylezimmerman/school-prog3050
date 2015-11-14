813de61368d294bcddb93760aff61f286c1e13c1
Sean Coombes <seancoombes@live.ca>
2015-11-14 14:37:06 -0500

    started refactoring tests

--- Veil/Veil.Tests/Controllers/GameProductsControllerTests.cs
+++ Veil/Veil.Tests/Controllers/GameProductsControllerTests.cs
@@ -268,23 +268,21 @@ namespace Veil.Tests.Controllers
         [TestCase(VeilRoles.ADMIN_ROLE)]
         public async void DeletePhysicalGameProduct_ValidDelete(string role)
         {
-            Game aGame = new Game();
-            aGame.Id = Id;
-            aGame.Name = "gameName";
-
-            GameProduct aGameProduct = new PhysicalGameProduct();
-            aGameProduct.GameId = aGame.Id;
-            aGameProduct.Id = new Guid("44B0752E-968B-477A-AAAD-3ED535BA3559");
-            aGameProduct.PlatformCode = ps4Platform.PlatformCode;
+            GameProduct aGameProduct = new PhysicalGameProduct()
+            {
+                GameId = game.Id,
+                Id = new Guid("44B0752E-968B-477A-AAAD-3ED535BA3559"),
+                PlatformCode = ps4Platform.PlatformCode,
+                Game = game,
+                Platform = ps4Platform,
+            };
 
             Mock<IVeilDataAccess> dbStub = TestHelpers.GetVeilDataAccessFake();
+
             Mock<DbSet<GameProduct>> gameProductDbSetStub = TestHelpers.GetFakeAsyncDbSet(new List<GameProduct> { aGameProduct }.AsQueryable());
-            MockPlatforms(dbStub);
-            gameProductDbSetStub.SetupForInclude();
 
             gameProductDbSetStub.Setup(gp => gp.FindAsync(Id)).ReturnsAsync(aGameProduct);
             dbStub.Setup(db => db.GameProducts).Returns(gameProductDbSetStub.Object);
-
 
             Mock<ControllerContext> contextStub = new Mock<ControllerContext>();
             contextStub.SetupUser().IsInRole(role);
@@ -313,7 +311,6 @@ namespace Veil.Tests.Controllers
 
             Mock<IVeilDataAccess> dbStub = TestHelpers.GetVeilDataAccessFake();
             Mock<DbSet<GameProduct>> gameProductDbSetStub = TestHelpers.GetFakeAsyncDbSet(new List<GameProduct> { gameSku }.AsQueryable());
-            gameProductDbSetStub.SetupForInclude();
 
             gameProductDbSetStub.Setup(gp => gp.FindAsync(Id)).ReturnsAsync(gameSku);
             dbStub.Setup(db => db.GameProducts).Returns(gameProductDbSetStub.Object);
@@ -454,31 +451,20 @@ namespace Veil.Tests.Controllers
         [TestCase(VeilRoles.ADMIN_ROLE)]
         public async void DeletePhysicalGameProductConfirmed_CatchesOnSaveDelete(string role)
         {
-            Game aGame = new Game();
-            aGame.Id = Id;
-            aGame.Name = "gameName";
-
             GameProduct aGameProduct = new PhysicalGameProduct();
-            aGameProduct.GameId = aGame.Id;
+            aGameProduct.GameId = game.Id;
             aGameProduct.Id = new Guid("44B0752E-968B-477A-AAAD-3ED535BA3559");
             aGameProduct.PlatformCode = ps4Platform.PlatformCode;
-            aGameProduct.Game = aGame;
+            aGameProduct.Game = game;
             aGameProduct.Platform = ps4Platform;
 
             Mock<IVeilDataAccess> dbStub = TestHelpers.GetVeilDataAccessFake();
 
-            Mock<DbSet<Game>> gameDbSetStub = TestHelpers.GetFakeAsyncDbSet(new List<Game> { aGame }.AsQueryable());
-            Mock<DbSet<GameProduct>> gameProductsDbSetStub = TestHelpers.GetFakeAsyncDbSet(new List<GameProduct> { aGameProduct }.AsQueryable());
-            MockPlatforms(dbStub);
-
-            gameProductsDbSetStub.SetupForInclude();
-
-            gameDbSetStub.Setup(g => g.FindAsync(aGame.Id)).ReturnsAsync(aGame);
+            Mock<DbSet<GameProduct>> gameProductsDbSetStub = TestHelpers.GetFakeAsyncDbSet(new List<GameProduct> { aGameProduct }.AsQueryable());           
+            gameProductsDbSetStub.SetupForInclude();            
             gameProductsDbSetStub.Setup(gp => gp.FindAsync(aGameProduct.Id)).ReturnsAsync(aGameProduct);
 
             dbStub.Setup(db => db.GameProducts).Returns(gameProductsDbSetStub.Object);
-            dbStub.Setup(db => db.Games).Returns(gameDbSetStub.Object);
-
             dbStub.Setup(db => db.SaveChangesAsync()).Throws<DbUpdateException>();
 
             Mock<ControllerContext> contextStub = new Mock<ControllerContext>();
