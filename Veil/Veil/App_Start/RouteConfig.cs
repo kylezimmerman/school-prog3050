309c835ad67e7de34d3dbabf5d3b76e1255a2351
Isaac West <bokthaku@gmail.com>
2015-11-17 13:31:05 -0500

    Added route for friendly Wishlist viewing, made alerts accessible

--- Veil/Veil/App_Start/RouteConfig.cs
+++ Veil/Veil/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
 using System.Web.Routing;
 
 namespace Veil
@@ -8,6 +9,32 @@ namespace Veil
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Wishlist",
+                url: "Wishlist/{action}/{itemId}",
+                defaults: new
+                {
+                    controller = "Wishlist",
+                    action = "Index",
+                    itemId = UrlParameter.Optional
+                },
+                constraints: new
+                {
+                    itemId = new GuidRouteConstraint()
+                }
+            );
+
+            routes.MapRoute(
+                name: "WishlistView",
+                url: "Wishlist/{username}",
+                defaults: new
+                {
+                    controller = "Wishlist",
+                    action = "Index",
+                    username = UrlParameter.Optional
+                }
+            );
 
             routes.MapRoute(
                 name: "Default",
