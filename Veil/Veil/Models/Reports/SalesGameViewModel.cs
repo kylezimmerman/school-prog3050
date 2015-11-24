1604858bc4d98a8f427fe4db8e390daa73f539c9
Isaac West <bokthaku@gmail.com>
2015-11-24 13:09:08 -0500

    Committing missing file

---
+++ Veil/Veil/Models/Reports/SalesGameViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Veil.DataModels.Models;
+
+namespace Veil.Models.Reports
+{
+    public class SalesGameViewModel
+    {
+        
+    }
+}
