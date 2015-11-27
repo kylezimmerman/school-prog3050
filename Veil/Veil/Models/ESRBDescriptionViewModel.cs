79fbcad88c891fda3447b64da2285cf992422d41
KyleZimmerman <zimmerkyle@gmail.com>
2015-11-27 12:36:43 -0500

    Started to Add ESRB Content Descriptors https://github.com/kylezimmerman/prog3050/issues/23

---
+++ Veil/Veil/Models/ESRBDescriptionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Veil.DataModels.Models;
+
+namespace Veil.Models
+{
+    public class ESRBDescriptionViewModel
+    {
+        public IEnumerable<ESRBContentDescriptor> All { get; set; }
+        public IEnumerable<ESRBContentDescriptor> Selected { get; set; }
+    }
+}
