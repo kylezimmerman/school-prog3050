b2aeae603ea08327d350400b15c9e7ba5163081a
Sean Coombes <seancoombes@live.ca>
2015-12-09 17:15:24 -0500

    #62 #64 tests finished and passing

--- Veil/Veil.Tests/Controllers/ManageControllerTests.cs
+++ Veil/Veil.Tests/Controllers/ManageControllerTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Security.Principal;
@@ -2555,6 +2556,164 @@ namespace Veil.Tests.Controllers
 
             Assert.That(result != null);
         }
+
+        [Test]
+        public async void ChangePassword_SuccessfulChange()
+        {
+
+            ChangePasswordViewModel passwordModel = new ChangePasswordViewModel()
+            {
+                OldPassword = "oldPassword",
+                NewPassword = "newPassword",
+                ConfirmPassword = "newPassword"
+            };
+            
+            User user = new User()
+            {
+                Id = memberId,
+                Email = "person@email.com",
+                NewEmail = "newEmail@email.com"
+            };
+
+            Mock<IVeilDataAccess> dbStub = TestHelpers.GetVeilDataAccessFake();
+            Mock<IAuthenticationManager> authenticationManagerStub = new Mock<IAuthenticationManager>();
+            Mock<IUserStore<User, Guid>> userStoreStub = new Mock<IUserStore<User, Guid>>();
+            Mock<VeilUserManager> userManagerStub = new Mock<VeilUserManager>(dbStub.Object, null /*messageService*/,
+                null /*dataProtectionProvider*/);
+            
+            userManagerStub.Setup(um => um.FindByIdAsync(memberId)).ReturnsAsync(user);
+            userManagerStub.Setup(um => um.ChangePasswordAsync(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            dbStub.Setup(db => db.UserStore).Returns(userStoreStub.Object);
+
+            Mock<VeilSignInManager> signInManagerMock = new Mock<VeilSignInManager>(userManagerStub.Object, authenticationManagerStub.Object);
+            signInManagerMock.Setup(sm => sm.SignInAsync(It.IsAny<User>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(Task.FromResult(0));
+
+            
+            
+            Mock<ControllerContext> context = new Mock<ControllerContext>();
+            context.Setup(c => c.HttpContext.User.Identity).Returns<IIdentity>(null);
+            context.Setup(c => c.HttpContext.User.Identity.IsAuthenticated).Returns(true);
+
+            Mock<IGuidUserIdGetter> idGetterStub = new Mock<IGuidUserIdGetter>();
+            idGetterStub.Setup(id => id.GetUserId(It.IsAny<IIdentity>())).Returns(memberId);
+          
+            ManageController controller = new ManageController(userManagerStub.Object, signInManagerMock.Object, dbStub.Object,
+                idGetterStub.Object, null)
+            {
+                ControllerContext = context.Object
+            };
+
+            var result = await controller.ChangePassword(passwordModel) as RedirectToRouteResult;
+
+            Assert.That(result != null);
+            Assert.That(result.RouteValues["action"], Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async void ChangePassword_InvalidModelState()
+        {
+            ChangePasswordViewModel passwordModel = new ChangePasswordViewModel();
+            ManageController controller = new ManageController(null, null, null, null, null);
+
+            controller.ModelState.AddModelError("error", "this is an error");
+
+            var result = await controller.ChangePassword(passwordModel) as ActionResult;
+
+            Assert.That(result != null);
+        }
+
+        [Test]
+        public async void ChangePassword_ThrowsDbEntityValidationException()
+        {
+
+            ChangePasswordViewModel passwordModel = new ChangePasswordViewModel()
+            {
+                NewPassword = "newPassword",
+                OldPassword = "oldPassword"
+            };
+
+            Mock<IVeilDataAccess> dbStub = TestHelpers.GetVeilDataAccessFake();
+            Mock<IUserStore<User, Guid>> userStoreStub = new Mock<IUserStore<User, Guid>>();
+            Mock<VeilUserManager> userManagerStub = new Mock<VeilUserManager>(dbStub.Object, null /*messageService*/,
+                null /*dataProtectionProvider*/);
+
+            userManagerStub.Setup(um => um.ChangePasswordAsync(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ThrowsAsync<VeilUserManager, IdentityResult>(new DbEntityValidationException());
+
+            dbStub.Setup(db => db.UserStore).Returns(userStoreStub.Object);
+
+            Mock<ControllerContext> context = new Mock<ControllerContext>();
+            context.Setup(c => c.HttpContext.User.Identity).Returns<IIdentity>(null);
+            context.Setup(c => c.HttpContext.User.Identity.IsAuthenticated).Returns(true);
+
+            Mock<IGuidUserIdGetter> idGetterStub = new Mock<IGuidUserIdGetter>();
+            idGetterStub.Setup(id => id.GetUserId(It.IsAny<IIdentity>())).Returns(memberId);
+
+            ManageController controller = new ManageController(userManagerStub.Object, null, dbStub.Object, idGetterStub.Object, null)
+            {
+                ControllerContext = context.Object
+            };
+
+            var result = await controller.ChangePassword(passwordModel) as ActionResult;
+
+            Assert.That(result != null);
+        }
+
+        [Test]
+        public async void ChangePassword_ResultEqualsIdentitiyResultFailed()
+        {
+
+            ChangePasswordViewModel passwordModel = new ChangePasswordViewModel()
+            {
+                OldPassword = "oldPassword",
+                NewPassword = "newPassword",
+                ConfirmPassword = "newPassword"
+            };
+
+            User user = new User()
+            {
+                Id = memberId,
+                Email = "person@email.com",
+                NewEmail = "newEmail@email.com"
+            };
+
+            Mock<IVeilDataAccess> dbStub = TestHelpers.GetVeilDataAccessFake();
+            Mock<IAuthenticationManager> authenticationManagerStub = new Mock<IAuthenticationManager>();
+            Mock<IUserStore<User, Guid>> userStoreStub = new Mock<IUserStore<User, Guid>>();
+            Mock<VeilUserManager> userManagerStub = new Mock<VeilUserManager>(dbStub.Object, null /*messageService*/,
+                null /*dataProtectionProvider*/);
+
+            userManagerStub.Setup(um => um.FindByIdAsync(memberId)).ReturnsAsync(user);
+            userManagerStub.Setup(um => um.ChangePasswordAsync(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Failed());
+
+            dbStub.Setup(db => db.UserStore).Returns(userStoreStub.Object);
+
+            Mock<VeilSignInManager> signInManagerMock = new Mock<VeilSignInManager>(userManagerStub.Object, authenticationManagerStub.Object);
+            signInManagerMock.Setup(sm => sm.SignInAsync(It.IsAny<User>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(Task.FromResult(0));
+
+
+
+            Mock<ControllerContext> context = new Mock<ControllerContext>();
+            context.Setup(c => c.HttpContext.User.Identity).Returns<IIdentity>(null);
+            context.Setup(c => c.HttpContext.User.Identity.IsAuthenticated).Returns(true);
+
+            Mock<IGuidUserIdGetter> idGetterStub = new Mock<IGuidUserIdGetter>();
+            idGetterStub.Setup(id => id.GetUserId(It.IsAny<IIdentity>())).Returns(memberId);
+
+            ManageController controller = new ManageController(userManagerStub.Object, signInManagerMock.Object, dbStub.Object,
+                idGetterStub.Object, null)
+            {
+                ControllerContext = context.Object
+            };
+
+            var result = await controller.ChangePassword(passwordModel) as ActionResult;
+
+            Assert.That(result != null);
+        }
+
         [Test]
         public async void ManagePlatforms_ReturnsMatchingModel()
         {
