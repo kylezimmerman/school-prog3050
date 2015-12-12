46136f48b3877222a5cf40fec4435f4c49b8052f
DAmatheson <row.blood.eagle@gmail.com>
2015-12-11 21:16:27 -0500

    Added doc comments to Global.asax.cs and Startup.cs

--- Veil/Veil/Global.asax.cs
+++ Veil/Veil/Global.asax.cs
@@ -1,4 +1,11 @@
-﻿using System.Web.Mvc;
+﻿/* Global.asax.cs
+ * Purpose: Setup the app and respond to application level events
+ * 
+ * Revision History:
+ *      Drew Matheson, 2015.09.25: Created
+ */
+
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Microsoft.Practices.Unity;
@@ -6,12 +13,19 @@ using Unity.Mvc5;
 
 namespace Veil
 {
+    /// <summary>
+    ///     Class for responding to application level events
+    /// </summary>
     public class MvcApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        ///     Application Start event which setups the application
+        /// </summary>
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
+            // Configure the app to use Unity for dependency injection
             IUnityContainer container = UnityConfig.GetConfiguredContainer();
 
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
--- Veil/Veil/Startup.cs
+++ Veil/Veil/Startup.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.Owin;
+﻿/* Startup.cs
+ * Purpose: 
+ * 
+ * Revision History:
+ *      Drew Matheson, 2015.09.25: Created
+ */ 
+
+using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Veil.Startup))]
+[assembly: OwinStartupAttribute(typeof (Veil.Startup))]
+
 namespace Veil
 {
+    /// <summary>
+    ///     OWIN startup and configuration class
+    /// </summary>
     public partial class Startup
     {
+        /// <summary>
+        ///     Configures the OWIN usage within the app
+        /// </summary>
+        /// <param name="app">
+        ///     The <see cref="IAppBuilder"/> to configure
+        /// </param>
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
